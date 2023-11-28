d9a09387b1fa31385216d871d9fc653f23abc65c
GODISGREATAMEN <mixi271020@gmail.com>
2023-11-28 21:02:23 +0200

    Change classes

--- src/game/LessThan3Game.java
+++ src/game/LessThan3Game.java
@@ -2,9 +2,9 @@ package game;
 
 public class LessThan3Game extends Game {
     public LessThan3Game(double bet) {
-
         super(bet);
     }
+
     public double calculateWinnings() {
         return 1.25 * userBet;
     }
--- src/game/MoreThan7Game.java
+++ src/game/MoreThan7Game.java
@@ -5,6 +5,7 @@ public class MoreThan7Game extends From3To7Game {
     public MoreThan7Game(double bet) {
         super(bet);
     }
+
     public double calculateWinnings() {
         return 2 * userBet - (0.1 * 2 * userBet);
     }
